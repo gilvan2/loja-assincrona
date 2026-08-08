7f4754593579839ebb5363dffde223092e82bd68
gilvan2 <gilvan.silva.junior@gmail.com>
2026-08-08 20:24:55 -0300

    adição das configurações do springboot + adi~ção de um banner bem legal ;)

---
+++ loja-api/src/main/java/com/loja_api/LojaApi.java
@@ -0,0 +1,11 @@
+package com.loja_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaApi {
+    public static void main(String[] args) {
+        SpringApplication.run(LojaApi.class, args);
+    }
+}
